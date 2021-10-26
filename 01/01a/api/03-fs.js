848dfe772eb3a55c3992631f8611f54fb2d76fe5
陶鸿杰 <18790315365@163.com>
2021-10-27 00:59:08 +0800

    1

--- 01/01a/api/03-fs.js
+++ 01/01a/api/03-fs.js
@@ -3,7 +3,12 @@ const { promisify } = require('util');
 
 const readFile = promisify(fs.readFile);
 
-fs.readFile('./conf.js', (err, d) => {
-  if (err) throw err;
-  console.log(d.toString());
+// fs.readFile('./conf.js', (err, d) => {
+//   if (err) throw err;
+//   console.log(d.toString());
+// });
+
+process.nextTick(async () => {
+  const data = await readFile('./conf.js');
+  console.log(data.toString());
 });
